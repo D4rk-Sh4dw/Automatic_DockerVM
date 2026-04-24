de2cbdaf5b9ca56bae9005aaae05193baaeee825
D4rk-Sh4dw <147989728+D4rk-Sh4dw@users.noreply.github.com>
2026-04-24 10:11:09 +0200

    Install Cron

--- setup.sh
+++ setup.sh
@@ -21,6 +21,7 @@ else
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
+    sudo apt install cron
     sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo usermod -aG docker $USER
     echo "Docker installed successfully!"
